fc4953ba398abe9137b9604da6bfe4b903607c10
RicardoBucard <ricardobucard@gmail.com>
2023-10-24 17:38:03 -0300

    application method to enforce authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :must_login
+  prepend_before_action :set_current_user
+
+  def must_login
+    return if @current_user.present?
+
+    redirect_to login_path
+  end
+
+  def set_current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
