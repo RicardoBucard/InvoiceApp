c819c6642037e89d232b7d65d0e4a7a200fb2563
RicardoBucard <ricardobucard@gmail.com>
2023-10-25 11:43:17 -0300

    change mailer method to send the new token instead of persisted one

--- app/mailers/register_mailer.rb
+++ app/mailers/register_mailer.rb
@@ -1,7 +1,7 @@
 class RegisterMailer < ApplicationMailer
-  def send_email(user)
+  def send_email(user, auth_token)
     @user = user
-
+    @auth_token = auth_token
     mail to: @user.email, subject: 'Validate your Invoice App access token'
   end
 end
--- lib/sends_confirmation_email.rb
+++ lib/sends_confirmation_email.rb
@@ -3,6 +3,6 @@ module SendsConfirmationEmail
 
   def sends_confirmation_email(email)
     return unless (user = find_or_create(email))
-    RegisterMailer.send_email(user).deliver_later
+    RegisterMailer.send_email(user, user.auth_token).deliver_later
   end
 end
