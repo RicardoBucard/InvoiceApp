79e71f070995328a9d2b46e0497c13a48c70ea28
RicardoBucard <ricardobucard@gmail.com>
2023-10-25 15:58:17 -0300

    fix factory_bot definition

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module DesafioNomad
     config.load_defaults 7.0
     config.autoload_paths << "#{Rails.root}/lib"
     config.eager_load_paths << "#{Rails.root}/lib"
+    config.factory_bot.definition_file_paths = ["custom/factories"]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
