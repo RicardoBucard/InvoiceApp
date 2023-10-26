75fe2d581c8c425a6bb9af4e60dcee215371d3c2
RicardoBucard <ricardobucard@gmail.com>
2023-10-26 04:10:55 -0300

    adds must_login error message alert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   def must_login
     return if @current_user.present?
 
-    redirect_to login_path
+    redirect_to login_path, alert: t("application.must_login_error")
   end
 
   def set_current_user
