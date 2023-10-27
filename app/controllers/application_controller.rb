db8231a04261d8243587d7f277d7dc3d3b4ac6a7
RicardoBucard <ricardobucard@gmail.com>
2023-10-27 02:19:09 -0300

    changes to allow postman requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :must_login
   prepend_before_action :set_current_user
+  protect_from_forgery with: :exception, if: Proc.new { |c| c.request.format != 'application/json' }
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
   def must_login
     return if @current_user.present?
