7c98dc7a6084cc6cf9194b83f19157bd761c2bf7
RicardoBucard <ricardobucard@gmail.com>
2023-10-27 08:21:36 -0300

    create invoice view and model  validation

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,6 +1,7 @@
 class Invoice < ApplicationRecord
   validates_presence_of :invoice_number, :invoice_date, :emitter_company, :charged_company, :invoice_value, :emails
   validates_uniqueness_of :invoice_number
+  validates :invoice_number, :invoice_value, numericality: { only_integer: true, greater_than: 0 }
   validates :invoice_date, presence: true, format: { with: /\A(\d{2})\/(\d{2})\/(\d{4})\z/, message: "Invalid date format" }
 
   scope :filter_invoice_date, ->(query) { where(invoice_date: query) }
