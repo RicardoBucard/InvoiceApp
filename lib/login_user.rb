d084acaf8e23b00c12f021744a95832028d0bcdb
RicardoBucard <ricardobucard@gmail.com>
2023-10-27 01:57:12 -0300

    login_user refactor

--- lib/login_user.rb
+++ lib/login_user.rb
@@ -3,7 +3,7 @@ module LoginUser
   include AuthenticateSession
 
   def login_user(auth_token)
-    user = find_user_by_auth_token(auth_token: auth_token)
+    user = find_user_by_auth_token(auth_token)
 
     redirect_to login_path, alert: t("logins.login.auth_token_not_found") and return unless user
     redirect_to login_path, alert: t("logins.login.unconfirmed_token") and return unless user.confirmed?
