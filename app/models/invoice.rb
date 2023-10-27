42271ae8c70aff4959593de8b2d449694007b360
RicardoBucard <ricardobucard@gmail.com>
2023-10-27 04:43:52 -0300

    adds more invoice model validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,5 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :invoice_number, :invoice_date, :emitter_company, :charged_company, :invoice_value, :emails
+  validates_uniqueness_of :invoice_number
+  validates :invoice_date, presence: true, format: { with: /\A(\d{2})\/(\d{2})\/(\d{4})\z/, message: "Invalid date format" }
 end
