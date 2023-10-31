dbf57a50c5b6e2389da792b16bcbf1057f8840c0
RicardoBucard <ricardobucard@gmail.com>
2023-10-31 00:45:33 -0300

    refactor user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id            :integer          not null, primary key
+#  auth_token    :string
+#  confirm_token :string
+#  email         :string
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_auth_token  (auth_token) UNIQUE
+#
 class User < ApplicationRecord
   has_secure_token :auth_token
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}, uniqueness: { case_sensitive: true }, presence: true
-
-  def email_token_activate
-    update_columns(confirm_token: nil)
-  end
 
   def confirmed?
     confirm_token.blank?
